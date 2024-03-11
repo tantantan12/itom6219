a43c1305948c46b3c25c1743df66e73439d6c7cd
Jane Xue Tan <janetanxue@gmail.com>
2024-03-11 10:16:48 -0500

    update

---
+++ Week 1/index.js
@@ -0,0 +1,5 @@
+document.getElementById("submit-btn").addEventListener("click", () => {
+    const productName = document.getElementById("name").value;
+    document.write('Welcome to ITOM 6219!')
+    console.log('I am hiding in the console!');
+    })
